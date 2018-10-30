2b9609d028e553e1a66531ff92a2b167dd10b8aa
Turkmen Mustafa Demirci <tmustafad@gmail.com>
2018-10-30 14:06:27 +0300

    initial commit

---
+++ src/main/java/com/turkmen/rest/concurrency/controller/KotlinBean.kt
@@ -0,0 +1,23 @@
+package com.turkmen.rest.concurrency.controller
+
+import org.springframework.stereotype.Component
+import java.util.concurrent.Callable
+
+
+@Component
+class KotlinBean {
+
+    fun helloKotlin(param: String): String {
+        println("A call from java to kotlin is being executed with param " + param)
+
+        return "Hi From Kotlin"
+    }
+
+
+    fun <T> getCallable(task: Callable<T>): Callable<T> {
+        return Callable {
+            task.call()
+
+        }
+    }
+}
---
+++ src/main/java/com/turkmen/rest/concurrency/controller/MainController.java
@@ -0,0 +1,105 @@
+package com.turkmen.rest.concurrency.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+@RestController
+public class MainController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    KotlinBean kotlinBean;
+
+
+    @RequestMapping(value = "/main")
+    public String getSuccess() {
+
+        long start = 0, end = 0;
+
+        System.out.println("Rest Call started .........." + String.valueOf(start = System.currentTimeMillis()));
+
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(restTemplate.getForObject("http://localhost:8080/callKotlin", String.class));
+        }
+
+        System.out.println("Rest Call finished .........." + String.valueOf(end = System.currentTimeMillis()));
+
+        System.out.println("Total time spent in seconds " + String.valueOf((end - start) / 1000));
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/callKotlin")
+    public String callKotlin() {
+
+        return kotlinBean.helloKotlin("Hello Kotlin");
+    }
+
+
+    @RequestMapping(value = "/callJavaCallable")
+    public String callJavaCallable() throws Exception {
+        long start = 0, end = 0;
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<String>> futures = new ArrayList<>();
+        System.out.println("Java Callable Rest Call started .........." + String.valueOf(start = System.currentTimeMillis()));
+        for (int i = 0; i < 10000; i++) {
+
+            Future<String> future = executorService.submit(() -> restTemplate.getForObject("http://localhost:8080/callKotlin", String.class));
+            futures.add(future);
+            //final CompletableFuture<String> uCompletableFuture = CompletableFuture
+            //      .supplyAsync(() -> restTemplate.getForObject("https://api.coinmarketcap.com/v2/listings", String.class));
+
+            //   System.out.println(uCompletableFuture.get());
+        }
+
+        for (Future<String> future : futures) {
+            System.out.println(future.get());
+        }
+
+        executorService.shutdown();
+        System.out.println("Java Callable Rest Call finished .........." + String.valueOf(end = System.currentTimeMillis()));
+
+        System.out.println("Java Callable Total time spent in seconds " + String.valueOf((end - start) / 1000));
+
+        return "success callable Java";
+    }
+
+
+    @RequestMapping(value = "/callKotlinCallable")
+    public String callKotlinCallable() throws Exception {
+
+        long start = 0, end = 0;
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<String>> futures = new ArrayList<>();
+        System.out.println("Kotlin Callable Rest Call started .........." + String.valueOf(start = System.currentTimeMillis()));
+        for (int i = 0; i < 10000; i++) {
+            Future<String> future = executorService.submit(
+                    new KotlinBean().getCallable(() -> restTemplate.getForObject("http://localhost:8080/callKotlin", String.class)));
+            futures.add(future);
+
+        }
+
+        for (Future<String> future : futures) {
+            System.out.println(future.get());
+        }
+
+        executorService.shutdown();
+        System.out.println("Kotlin Callable Rest Call finished .........." + String.valueOf(end = System.currentTimeMillis()));
+
+        System.out.println("Kotlin Callable Total time spent in seconds " + String.valueOf((end - start) / 1000));
+
+
+        return "success callable Kotlin";
+    }
+
+
+}
